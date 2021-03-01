0ac7824fa384dbcb34fbd32d2e7d477882604301
stanleyoos <stanislaw.sarka@gmail.com>
2021-03-01 12:12:42 +0100

    Added actions and data validation

--- actions/noteActions.js
+++ actions/noteActions.js
@@ -1,36 +1,51 @@
 const Note = require("../db/models/note");
 
 class NoteActions {
-  saveNote(req, res) {
-    //   const newNote = new Note({
-    //     title: "Zrobić zakupy",
-    //     body: "Mleko, płatki",
-    //   });
-
-    //   newNote.save().then(() => console.log("notatka została dodana"));
-
-    //   res.send("Strona główna działa");
+  async saveNote(req, res) {
     const title = req.body.title;
     const body = req.body.body;
 
-    res.send("Note: " + title + "  " + body);
+    let note;
+
+    try {
+      note = new Note({ title, body });
+      await note.save();
+    } catch (err) {
+      return res.status(422).json({ message: err.message });
+    }
+
+    res.status(201).json(note);
   }
-  getAllNotes(req, res) {
-    res.send("Api działa    !!!!!");
+  async getAllNotes(req, res) {
+    const doc = await Note.find({});
+
+    res.status(200).json(doc);
   }
 
-  getNote(req, res) {
+  async getNote(req, res) {
     const id = req.params.id;
-    res.send("Info o notatce " + id);
+    const note = await Note.findOne({ _id: id });
+    res.status(200).json(note);
   }
 
-  updateNote(req, res) {
-    res.send("Notatka została zaktualizowana");
-  }
-
-  deleteNote(req, res) {
+  async updateNote(req, res) {
     const id = req.params.id;
-    res.send("Notatka " + id + " zostala usunięta");
+    const title = req.body.title;
+    const body = req.body.body;
+
+    const note = await Note.findOne({ _id: id });
+    note.title = title;
+    note.body = body;
+
+    await note.save();
+
+    res.status(200).json(note);
+  }
+
+  async deleteNote(req, res) {
+    const id = req.params.id;
+    await Note.deleteOne({ _id: id });
+    res.sendStatus(204);
   }
 }
 
